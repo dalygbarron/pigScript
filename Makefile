cb300ce337f5d677f5f9afd685527e8a384f16d6
marineorganism <marineorganism@gmail.com>
2015-12-26 17:03:16 +1100

    making it work as a static library

--- Makefile
+++ Makefile
@@ -1,12 +1,24 @@
 CC = g++
-FLAGS = -static -std=c++11 -g
+FLAGS = -static -std=c++11
 LIBS = -L /usr/local/lib/danylib/ -l danylib
 
 assembler: src/*.cc src/assembler/*.cc
 	$(CC) $(FLAGS) src/assembler/*.cc src/Script.cc src/Instruction.cc $(LIBS) -o bin/assembler
 
-pigScriptVanilla: src/*.cc
-	$(CC) $(FLAGS) src/*.cc $(LIBS) -o bin/pigScriptVanilla
+virtualMachine:
+	$(CC) $(FLAGS) src/Instruction.cc $(LIBS) -c
+	$(CC) $(FLAGS) src/Script.cc $(LIBS) -c
+	$(CC) $(FLAGS) src/VirtualMachine.cc $(LIBS) -c
+	ar rcs bin/libpigScript.a *.o
+	rm *.o
 
-install: assembler
-	echo "this doesn't work yet"
+install: assembler virtualMachine
+	#make the directories they go in
+	mkdir -p /usr/local/bin/
+	mkdir -p /usr/local/lib/pigScript/
+	mkdir -p /usr/local/include/pigScript/
+
+	#copy the files over
+	cp bin/assembler /usr/local/bin/pigScript/pigScriptAssembler
+	cp src/*.hh /usr/local/include/pigScript/
+	cp bin/libpigScript.a /usr/local/lib/pigScript/libpigScript.a
--- src/Instruction.cc
+++ src/Instruction.cc
@@ -7,32 +7,29 @@
 
 OpCode stringToOpCode(char const * token)
 {
-  if (strcmp(token,"jmp") == 0) return op_jmp;
-  else if (strcmp(token,"jeq") == 0) return op_jeq;
-  else if (strcmp(token,"jne") == 0) return op_jne;
-  else if (strcmp(token,"jlt") == 0) return op_jlt;
-  else if (strcmp(token,"jlg") == 0) return op_jle;
-  else if (strcmp(token,"jgt") == 0) return op_jgt;
-  else if (strcmp(token,"jge") == 0) return op_jge;
-  else if (strcmp(token,"call") == 0) return op_call;
-  else if (strcmp(token,"set") == 0) return op_set;
-  else if (strcmp(token,"add") == 0) return op_add;
-  else if (strcmp(token,"sub") == 0) return op_sub;
-  else if (strcmp(token,"move") == 0) return op_move;
-  else if (strcmp(token,"dump") == 0) return op_dump;
-  else if (strcmp(token,"end") == 0) return op_end;
-  else if (strcmp(token,"label") == 0) return label;
-  else
-  {
-    return invalid;
-  }
+	if (strcmp(token,"jmp") == 0) return op_jmp;
+	else if (strcmp(token,"jeq") == 0) return op_jeq;
+	else if (strcmp(token,"jne") == 0) return op_jne;
+	else if (strcmp(token,"jlt") == 0) return op_jlt;
+	else if (strcmp(token,"jlg") == 0) return op_jle;
+ 	else if (strcmp(token,"jgt") == 0) return op_jgt;
+	else if (strcmp(token,"jge") == 0) return op_jge;
+	else if (strcmp(token,"call") == 0) return op_call;
+	else if (strcmp(token,"set") == 0) return op_set;
+	else if (strcmp(token,"add") == 0) return op_add;
+	else if (strcmp(token,"sub") == 0) return op_sub;
+	else if (strcmp(token,"move") == 0) return op_move;
+	else if (strcmp(token,"dump") == 0) return op_dump;
+	else if (strcmp(token,"end") == 0) return op_end;
+	else if (strcmp(token,"label") == 0) return label;
+	else return invalid;
 }
 
 
 Instruction::Instruction(OpCode pCode):
 code(pCode)
 {
-  args = nullptr;
+	args = NULL;
 }
 
 
@@ -44,101 +41,101 @@ args(pArgs)
 
 Instruction::Instruction(FILE * inFile)
 {
-  code = op_jmp;
-  fread(&code,OP_CODE_SIZE,1,inFile);
+	code = op_jmp;
+	fread(&code,OP_CODE_SIZE,1,inFile);
 
-  switch(code)
-  {
-  case op_jmp:
-    args = new uint8_t[LABEL_SIZE];
-    fread(args,LABEL_SIZE,1,inFile);
-    break;
+	switch(code)
+	{
+	case op_jmp:
+		args = new uint8_t[LABEL_SIZE];
+		fread(args,LABEL_SIZE,1,inFile);
+		break;
 
-  case op_jeq:
-  case op_jne:
-  case op_jlt:
-  case op_jle:
-  case op_jgt:
-  case op_jge:
-    args = new uint8_t[LABEL_SIZE + VAR_SIZE];
-    fread(args,LABEL_SIZE + VAR_SIZE,1,inFile);
-    break;
+	case op_jeq:
+	case op_jne:
+	case op_jlt:
+	case op_jle:
+	case op_jgt:
+	case op_jge:
+		args = new uint8_t[LABEL_SIZE + VAR_SIZE];
+		fread(args,LABEL_SIZE + VAR_SIZE,1,inFile);
+		break;
 
-  case op_set:
-    args = new uint8_t[CONST_SIZE + VAR_SIZE];
-    fread(args,CONST_SIZE + VAR_SIZE,1,inFile);
-    break;
+	case op_set:
+		args = new uint8_t[CONST_SIZE + VAR_SIZE];
+		fread(args,CONST_SIZE + VAR_SIZE,1,inFile);
+		break;
 
-  case op_move:
-    args = new uint8_t[VAR_SIZE * 2];
-    fread(args,VAR_SIZE * 2,1,inFile);
-    break;
+	case op_move:
+		args = new uint8_t[VAR_SIZE * 2];
+		fread(args,VAR_SIZE * 2,1,inFile);
+		break;
 
-  case op_add:
-  case op_sub:
-    args = new uint8_t[3];
-    fread(args,VAR_SIZE * 3,1,inFile);
-    break;
+	case op_add:
+	case op_sub:
+		args = new uint8_t[3];
+		fread(args,VAR_SIZE * 3,1,inFile);
+		break;
 
-  case op_call:
-    int argLength;
-    fread(&argLength,2,1,inFile);
-    args = new uint8_t[argLength];
-    args[0] = argLength & 0xFF;
-    args[1] = (argLength >> 8) & 0xFF;
-    fread(args + 2,1,argLength - 2,inFile);
-    break;
-  }
+	case op_call:
+		int argLength;
+		fread(&argLength,2,1,inFile);
+		args = new uint8_t[argLength];
+		args[0] = argLength & 0xFF;
+		args[1] = (argLength >> 8) & 0xFF;
+		fread(args + 2,1,argLength - 2,inFile);
+		break;
+	}
 }
 
 
 Instruction::~Instruction()
 {
-  if (args != nullptr)
-  {
-    delete[] args;
-    args = nullptr;
-  }
+	if (args != NULL)
+	{
+		delete[] args;
+		args = NULL;
+	}
 }
 
 
 void Instruction::toFile(FILE * outFile)
 {
-  printf("%d\n",code);
+	printf("%d\n",code);
 
-  fwrite(&code,OP_CODE_SIZE,1,outFile);
+	fwrite(&code,OP_CODE_SIZE,1,outFile);
 
-  switch(code)
-  {
-  case op_jmp:
-    fwrite(args,1,LABEL_SIZE,outFile);
-    break;
+	switch(code)
+	{
+	case op_jmp:
+		fwrite(args,1,LABEL_SIZE,outFile);
+		break;
 
-  case op_jeq:
-  case op_jne:
-  case op_jlt:
-  case op_jle:
-  case op_jgt:
-  case op_jge:
-    fwrite(args,1,LABEL_SIZE + VAR_SIZE,outFile);
-    break;
+	case op_jeq:
+	case op_jne:
+	case op_jlt:
+	case op_jle:
+	case op_jgt:
+	case op_jge:
+		fwrite(args,1,LABEL_SIZE + VAR_SIZE,outFile);
+		break;
 
-  case op_set:
-    fwrite(args,1,CONST_SIZE + VAR_SIZE,outFile);
-    break;
+	case op_set:
+		fwrite(args,1,CONST_SIZE + VAR_SIZE,outFile);
+		break;
 
-  case op_move:
-    fwrite(args,1,VAR_SIZE * 2,outFile);
-    break;
+	case op_move:
+		fwrite(args,1,VAR_SIZE * 2,outFile);
+		break;
 
-  case op_add:
-  case op_sub:
-    fwrite(args,1,VAR_SIZE * 3,outFile);
-    break;
+	case op_add:
+	case op_sub:
+		fwrite(args,1,VAR_SIZE * 3,outFile);
+		break;
 
-  case op_call:
-    int argLength = args[0] | (args[1] << 8);
-    fwrite(args,1,argLength,outFile);
-    break;
-  }
+	case op_call:
+		int argLength = args[0] | (args[1] << 8);
+		fwrite(args,1,argLength,outFile);
+		break;
+	}
 }
